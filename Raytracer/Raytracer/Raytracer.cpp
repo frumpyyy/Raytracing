31a30bd31e867486efa91358d48b7a6b73672bd4
Nathan Moody <146941482+frumpyyy@users.noreply.github.com>
2026-04-05 15:08:00 +0100

    Sphere colouring using ray casting that draws normals

--- Raytracer/Raytracer/Raytracer.cpp
+++ Raytracer/Raytracer/Raytracer.cpp
@@ -1,6 +1,25 @@
 ﻿#include "Raytracer.h"
 
+double hit_sphere(const point3& center, double radius, const Ray& r) {
+	Vector3 oc = center - r.origin();
+	double a = Dot(r.direction(), r.direction());
+	double b = -2.0 * Dot(r.direction(), oc);
+	double c = Dot(oc, oc) - radius * radius;
+	double discriminant = b * b - 4 * a * c;
+
+	if (discriminant < 0)
+		return -1.0;
+	else
+		return (-b - std::sqrt(discriminant)) / (2.0 * a);
+}
+
 colour ray_colour(const Ray& r) {
+	double t = hit_sphere(point3(0, 0, -1), 0.5, r);
+	if (t > 0.0) {
+		Vector3 normal = UnitVector(r.at(t) - Vector3(0, 0, -1));
+		return 0.5 * colour(normal.x() + 1, normal.y() + 1, normal.z() + 1);
+	}
+
 	Vector3 unitDirection = UnitVector(r.direction());
 	auto a = 0.5 * (unitDirection.y() + 1.0);
 	return (1.0 - a) * colour(1.0, 1.0, 1.0) + a * colour(0.5, 0.7, 1.0);
